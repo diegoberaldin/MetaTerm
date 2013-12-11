ee510e7b7e66f6ea66171c3954fd1858d175cbf7
Diego Beraldin <diego.beraldin@gmail.com>
2013-12-11 18:05:33 +0100

    set up logging once for all

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
-#
-# src/main.py
-#
-# This is the main application entry point.
+
+"""
+.. currentmodule:: src.main
+
+This module is the main application entry point.
+"""
+
+import logging.config
 
 from src import model
 
-if __name__ == '__main__':
-    model.initialize_tb_folder()
 
+def initialize_logging():
+    """Initializes the logging module for use throughout the whole application, reading the configuration from the
+    ``logging.conf`` file located at the same level as the main module.
+
+    :rtype: None
+    """
+    logging.config.fileConfig('logging.conf')
+
+if __name__ == '__main__':
+    initialize_logging()
+    model.initialize_tb_folder()
+    tb = model.TermBase('prova')
+    tb.schema.add_property('Domain', 'E')
