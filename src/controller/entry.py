ed193a9f5ac992ec28efdb05dbea3d83711843ee
Diego Beraldin <diego.beraldin@gmail.com>
2013-12-27 16:14:08 +0100

    implemented entry creation in the controller

--- src/controller/entry.py
+++ src/controller/entry.py
@@ -6,6 +6,7 @@
 """
 
 from src.controller.abstract import AbstractController
+from src import model as mdl
 
 
 class EntryController(AbstractController):
@@ -24,4 +25,27 @@ class EntryController(AbstractController):
 
     def _handle_save_entry(self):
         form = self._view.entry_display.content
+        # creates the new entry
+        new_entry = mdl.get_main_model().open_termbase.create_entry()
+        for (property_id,
+             value) in form.get_entry_level_property_values().items():
+            # inserts entry-level properties
+            new_entry.set_property(property_id, value)
+        for ((language_id, property_id),
+             value) in form.get_language_level_property_values().items():
+            # insert language-level properties
+            new_entry.set_language_property(language_id, property_id, value)
+        for (locale, lemmata) in form.get_terms().items():
+            # inserts all terms
+            assert len(lemmata) == 1
+            lemma = lemmata.pop()
+            new_entry.add_term(lemma, locale, True)
+        for ((locale, lemma, property_id),
+             value) in form.get_term_level_property_values().items():
+            # inserts term properties
+            term = new_entry.get_term(locale, lemma)
+            term.set_property(property_id, value)
+        self._view.entry_display.display_welcome_screen()
+
+
 
