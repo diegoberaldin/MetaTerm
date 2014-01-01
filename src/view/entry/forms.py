diegoberaldin/MetaTerm

commit e3fea7421e458357f99c1b8d2dc8e0923b57f572

added basic validity check to entry manipulation forms - fixes #10

src/view/entry/forms.py
@@ -12,9 +12,6 @@ from PyQt4 import QtCore, QtGui
 from src import model as mdl
 from src.view.entry import fields
 
-# TODO: some refactoring is still needed!
-# I'd like to better isolate the forms and the CustomMenuXXXWidgets
-
 
 class AbstractEntryForm(QtGui.QWidget):
     """Base class used for entry creation and update forms, providing the
@@ -168,6 +165,22 @@ class AbstractEntryForm(QtGui.QWidget):
         """
         raise NotImplementedError('Override me!')
 
+    @property
+    def is_valid(self):
+        """Checks whether the content of the form is acceptable.
+
+        :returns: True if the content or the form is acceptable and can be
+        saved, False otherwise
+        :rtype: bool
+        """
+        term_dict = self.get_terms()
+        # checks that there are no (language, lemma) duplicates within the entry
+        for locale in term_dict.keys():
+            if any((lemma1 == lemma2 for lemma1 in term_dict[locale] for lemma2
+                    in term_dict[locale])):
+                return False
+        return True
+
 
 class CreateEntryForm(AbstractEntryForm):
     """Widget used to represent the form which is to be displayed whenever the
@@ -206,10 +219,13 @@ class CreateEntryForm(AbstractEntryForm):
             self._populate_fields('L', language_property_layout, locale)
             language_widget.layout().addLayout(language_property_layout)
             # creates the term widget and inserts term-level fields
-            term_widget = CustomMenuTermWidget(self, '', True, self)
+            term_widget = CustomMenuTermWidget(locale, '', True, self)
             self._term_widgets[locale].append(term_widget)
             term_label = QtGui.QLabel('<strong>Term</strong>', self)
             term_input = QtGui.QLineEdit(self)
+            # needed to record changes in the term fields
+            term_input.textEdited.connect(
+                lambda unused_text: self._handle_entry_changed())
             # needed to keep consistency
             term_input.textChanged.connect(term_widget.update_lemma)
             term_widget.layout().addRow(term_label, term_input)
@@ -274,12 +290,15 @@ class UpdateEntryForm(AbstractEntryForm):
                 term_label = QtGui.QLabel('<strong>Term</strong>', self)
                 term_input = QtGui.QLineEdit(self)
                 term_input.setText(term.lemma)
+                # needed to record changes in the term fields
+                term_input.textEdited.connect(
+                    lambda unused_text: self._handle_entry_changed())
                 # needed to keep consistency
                 term_input.textChanged.connect(term_widget.update_lemma)
                 term_widget.layout().addRow(term_label, term_input)
                 self._populate_fields('T', term_widget.layout(), locale,
                                       term.lemma)
-                # needed to keep field bound to the term in the input field
+                # needed to keep fields bound to the term in the input field
                 for field in [f for f in self._fields if
                               f.level == 'T' and f.locale == locale
                               and f.lemma == term.lemma]:
@@ -335,6 +354,9 @@ class UpdateEntryForm(AbstractEntryForm):
         term_widget = CustomMenuTermWidget(locale, '', False, self)
         term_label = QtGui.QLabel('<strong>Term</strong>', self)
         term_input = QtGui.QLineEdit(self)
+        # needed to record changes in the term fields
+        term_input.textEdited.connect(
+            lambda unused_text: self._handle_entry_changed())
         term_widget.layout().addRow(term_label, term_input)
         # keeps the link between the term widget and the input field
         term_input.textChanged.connect(term_widget.update_lemma)

src/controller/entry.py
@@ -7,6 +7,8 @@ This module contains the controller that governs entry creation, update and
 deletion and manages entry visualization in the graphical user interface.
 """
 
+from PyQt4 import QtGui
+
 from src.controller.abstract import AbstractController
 from src import model as mdl
 
@@ -65,6 +67,17 @@ class EntryController(AbstractController):
         :rtype: None
         """
         form = self._view.entry_display.content
+        if not form.is_valid:
+            message = QtGui.QMessageBox()
+            message.setIcon(QtGui.QMessageBox.Critical)
+            message.setText('Invalid entry')
+            message.setInformativeText('Check that all the mandatory fields '
+                                       'are filled-in and that there are not '
+                                       'terms with the same lemma within the '
+                                       'same language in this entry.')
+            message.exec()
+            # don't do anything of the rest
+            return
         if form.is_new:
             # creates the new entry
             entry = mdl.get_main_model().open_termbase.create_entry()

commit 1b78052f833d3a893e235dc4b3d4ae47ccdc38a8

fixed very stupid bug introduced by e3fea74

src/view/entry/forms.py
@@ -7,6 +7,8 @@ This module contains the definition of the forms that are used in order to
 create new terminological entries or to modify existing ones.
 """
 
+import collections
+
 from PyQt4 import QtCore, QtGui
 
 from src import model as mdl
@@ -176,8 +178,8 @@ class AbstractEntryForm(QtGui.QWidget):
         term_dict = self.get_terms()
         # checks that there are no (language, lemma) duplicates within the entry
         for locale in term_dict.keys():
-            if any((lemma1 == lemma2 for lemma1 in term_dict[locale] for lemma2
-                    in term_dict[locale])):
+            if any(count > 1 for count
+                   in collections.Counter(term_dict[locale]).values()):
                 return False
         return True
 