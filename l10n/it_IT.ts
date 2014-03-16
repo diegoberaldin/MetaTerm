ef5b77511c727352b712f0cc701337ee1d808b41
Diego Beraldin <diego.beraldin@gmail.com>
2014-03-16 20:39:40 +0100

    added forgot files and links to version control (this should have been done in the previous commit)

---
+++ l10n/it_IT.ts
@@ -0,0 +1,275 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>CreateEntryForm</name>
+    <message>
+        <location filename="src/view/entry/forms.py" line="245"/>
+        <source>&lt;strong&gt;Term&lt;/strong&gt;</source>
+        <translation>&lt;strong&gt;Termine&lt;/strong&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>CustomMenuLanguageWidget</name>
+    <message>
+        <location filename="src/view/entry/forms.py" line="443"/>
+        <source>Add term</source>
+        <translation>Nuovo termine</translation>
+    </message>
+    <message>
+        <location filename="src/view/entry/forms.py" line="484"/>
+        <source>Language menu</source>
+        <translation>Lingua</translation>
+    </message>
+</context>
+<context>
+    <name>CustomMenuTermWidget</name>
+    <message>
+        <location filename="src/view/entry/forms.py" line="532"/>
+        <source>Delete term</source>
+        <translation>Cancella termine</translation>
+    </message>
+    <message>
+        <location filename="src/view/entry/forms.py" line="550"/>
+        <source>Term menu</source>
+        <translation>Termine</translation>
+    </message>
+</context>
+<context>
+    <name>DefaultLanguages</name>
+    <message>
+        <location filename="src/view/enum.py" line="66"/>
+        <source>English (Unites States)</source>
+        <translation>Inglese (Stati Uniti)</translation>
+    </message>
+    <message>
+        <location filename="src/view/enum.py" line="68"/>
+        <source>English (United Kingdom)</source>
+        <translation>Inglese (Regno Unito)</translation>
+    </message>
+    <message>
+        <location filename="src/view/enum.py" line="70"/>
+        <source>Spanish (Spain)</source>
+        <translation>Spagnolo (Spagna)</translation>
+    </message>
+    <message>
+        <location filename="src/view/enum.py" line="72"/>
+        <source>French (France)</source>
+        <translation>Francese (Francia)</translation>
+    </message>
+    <message>
+        <location filename="src/view/enum.py" line="74"/>
+        <source>Italian (Italy)</source>
+        <translation>Italiano (Italia)</translation>
+    </message>
+    <message>
+        <location filename="src/view/enum.py" line="76"/>
+        <source>Romanian</source>
+        <translation>Romeno</translation>
+    </message>
+    <message>
+        <location filename="src/view/enum.py" line="78"/>
+        <source>Greek</source>
+        <translation>Greco</translation>
+    </message>
+</context>
+<context>
+    <name>EntryScreen</name>
+    <message>
+        <location filename="src/view/entry/widgets.py" line="344"/>
+        <source>&lt;small&gt;Entry ID: {0}&lt;/small&gt;</source>
+        <translation>&lt;small&gt;ID scheda: {0}&lt;/small&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>LanguageSelector</name>
+    <message>
+        <location filename="src/view/entry/widgets.py" line="187"/>
+        <source>Language</source>
+        <translation>Lingua</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/view/main.py" line="80"/>
+        <source>MetaTerm</source>
+        <translation>MetaTerm</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="82"/>
+        <source>Started.</source>
+        <translation>Avviato.</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="127"/>
+        <source>Termbase</source>
+        <translation>Termbase</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="139"/>
+        <source>Entry</source>
+        <translation>Scheda</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="147"/>
+        <source>?</source>
+        <translation>?</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="165"/>
+        <source>Warning</source>
+        <translation>Avviso</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="166"/>
+        <source>Are you sure you want to delete this termbase?</source>
+        <translation>Eliminare davvero questo termbase?</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="168"/>
+        <source>The operation cannot be undone, do you still want to proceed?</source>
+        <translation>L&apos;operazione non potrà essere annullata. Continuare?</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="262"/>
+        <source>New termbase...</source>
+        <translation>Nuovo termbase...</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="267"/>
+        <source>Open termbase...</source>
+        <translation>Apri termbase...</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="271"/>
+        <source>Close termbase...</source>
+        <translation>Chiudi termbase...</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="277"/>
+        <source>Delete termbase...</source>
+        <translation>Elimina termbase...</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="280"/>
+        <source>Export...</source>
+        <translation>Esporta...</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="286"/>
+        <source>Termbase properties...</source>
+        <translation>Proprietà...</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="292"/>
+        <source>Insert entry</source>
+        <translation>Inserisci scheda</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="297"/>
+        <source>Save entry</source>
+        <translation>Salva scheda</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="302"/>
+        <source>Edit entry</source>
+        <translation>Modifica scheda</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="307"/>
+        <source>Cancel edit</source>
+        <translation>Annulla modifiche</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="313"/>
+        <source>Delete entry</source>
+        <translation>Elimina scheda</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="319"/>
+        <source>Quit</source>
+        <translation>Chiudi</translation>
+    </message>
+    <message>
+        <location filename="src/view/main.py" line="324"/>
+        <source>About Qt</source>
+        <translation>Informazioni su Qt</translation>
+    </message>
+</context>
+<context>
+    <name>SelectFileInput</name>
+    <message>
+        <location filename="src/view/entry/fields.py" line="68"/>
+        <source>Browse</source>
+        <translation>Sfoglia</translation>
+    </message>
+    <message>
+        <location filename="src/view/entry/fields.py" line="70"/>
+        <source>Clear</source>
+        <translation>Pulisci input</translation>
+    </message>
+    <message>
+        <location filename="src/view/entry/fields.py" line="122"/>
+        <source>Choose file</source>
+        <translation>Seleziona file</translation>
+    </message>
+</context>
+<context>
+    <name>SelectTermbaseDialog</name>
+    <message>
+        <location filename="src/view/dialogs.py" line="50"/>
+        <source>Select termbase</source>
+        <translation>Seleziona termbase</translation>
+    </message>
+    <message>
+        <location filename="src/view/dialogs.py" line="54"/>
+        <source>Please select a termbase from the list below:</source>
+        <translation>Seleziona un termbase dalla lista:</translation>
+    </message>
+</context>
+<context>
+    <name>TermbasePropertyDialog</name>
+    <message>
+        <location filename="src/view/dialogs.py" line="102"/>
+        <source>Termbase properties</source>
+        <translation>Proprietà del termbase</translation>
+    </message>
+    <message>
+        <location filename="src/view/dialogs.py" line="120"/>
+        <source>Languages</source>
+        <translation>Lingue</translation>
+    </message>
+    <message>
+        <location filename="src/view/dialogs.py" line="131"/>
+        <source>Statistics</source>
+        <translation>Statistiche</translation>
+    </message>
+    <message>
+        <location filename="src/view/dialogs.py" line="133"/>
+        <source>Number of entries:</source>
+        <translation>Numero di schede:</translation>
+    </message>
+    <message>
+        <location filename="src/view/dialogs.py" line="139"/>
+        <source>Total size:</source>
+        <translation>Dimensione totale:</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateEntryForm</name>
+    <message>
+        <location filename="src/view/entry/forms.py" line="377"/>
+        <source>&lt;strong&gt;Term&lt;/strong&gt;</source>
+        <translation>&lt;strong&gt;Termine&lt;/strong&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomeScreen</name>
+    <message>
+        <location filename="src/view/entry/widgets.py" line="442"/>
+        <source>&lt;h1&gt;Welcome to MetaTerm!&lt;/h1&gt;&lt;strong&gt;MetaTerm&lt;/strong&gt; is an application that helps you creating and maintaining terminological databases .&lt;br /&gt; If you want to create a new termbase go to &lt;code&gt;Termbase &amp;gt; New...&lt;/code&gt; in order to start the creation wizard.&lt;br /&gt; To open an existing termbase go to &lt;code&gt;Termbase &amp;gt; Open...&lt;/code&gt; and choose one of the available items.</source>
+        <translation>&lt;h1&gt;Benvenuti su MetaTerm!&lt;/h1&gt;&lt;strong&gt;MetaTerm&lt;/strong&gt; è un&apos;applicazione per la creazione e la gestione di banche dati terminologiche (termbase).&lt;br /&gt; Per creare un nuovo termbase, selezionando &lt;code&gt;Termbase &amp;gt; Nuovo...&lt;/code&gt;si aprirà la procedura guidata di creazione di un nuovo termbase.&lt;br /&gt; Per aprire un termbase esistente, selezionare &lt;code&gt;Termbase &amp;gt; Apri...&lt;/code&gt; e scegliere uno dei termbase disponibili.</translation>
+    </message>
+</context>
+</TS>
