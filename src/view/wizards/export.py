1e4d8f1e4db76909d6b672840977502c71921c36
Diego Beraldin <diego.beraldin@gmail.com>
2014-03-29 09:44:37 +0100

    implemented field selection in the termbase export wizard

--- src/view/wizards/export.py
+++ src/view/wizards/export.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # MetaTerm - A terminology management application written in Python
-# Copyright (C) 2013 Diego Beraldin
+# Copyright (C) 2014 Diego Beraldin
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@ from src import model as mdl
 
 
 class ExportWizard(QtGui.QWizard):
-    TYPE_PAGE, LANGUAGE_PAGE, FIELD_PAGE, FINAL_PAGE = range(4)
+    TYPE_PAGE, LANGUAGE_PAGE, THIRD_FIELD_PAGE, FINAL_PAGE = range(4)
     """Constants used as page IDs in order to create this non-linear wizard.
     """
 
@@ -56,7 +56,7 @@ class ExportWizard(QtGui.QWizard):
         # adds the wizard pages
         self.setPage(self.TYPE_PAGE, ExportTypePage(self))
         self.setPage(self.LANGUAGE_PAGE, LanguagePage(self))
-        self.setPage(self.FIELD_PAGE, FieldSelectionPage(self))
+        self.setPage(self.THIRD_FIELD_PAGE, ThirdFieldSelectionPage(self))
         self.setPage(self.FINAL_PAGE, FinalPage(self))
         self.resize(self._WIDTH, self._HEIGHT)
         self.show()
@@ -93,18 +93,13 @@ class LanguagePage(QtGui.QWizardPage):
     def __init__(self, parent):
         super(LanguagePage, self).__init__(parent)
         self._languages = DefaultLanguages(self)
-        # title and subtitle
+        # page title
         self.setTitle(self.tr('Select language pair'))
-        if self.field('csv_type'):
-            subtitle = self.tr('Please select the two languages you wish to '
-                               'export to the CSV file.')
-        else:
-            subtitle = self.tr('Please select the two languages you wish to '
-                               'export to the TSV file.')
-        self.setTitle(subtitle)
+        # language selection widgets
         self._available_languages_view = QtGui.QListWidget(self)
         self._chosen_languages = QtGui.QListWidget(self)
         self._populate_language_views()
+        # button widget
         button_widget = QtGui.QWidget(self)
         button_widget.setLayout(QtGui.QVBoxLayout(button_widget))
         select_button = QtGui.QToolButton(button_widget)
@@ -121,10 +116,20 @@ class LanguagePage(QtGui.QWizardPage):
         self.layout().addWidget(button_widget)
         self.layout().addWidget(self._chosen_languages)
 
+    def initializePage(self):
+        # shows the correct subtitle
+        if self.field('csv_type'):
+            subtitle = self.tr('Please select the two languages you wish to '
+                               'export to the CSV file.')
+        else:
+            subtitle = self.tr('Please select the two languages you wish to '
+                               'export to the TSV file.')
+        self.setSubTitle(subtitle)
+
     def _populate_language_views(self):
         termbase_locales = mdl.get_main_model().open_termbase.languages
         for locale in termbase_locales:
-            flag = QtGui.QIcon(':/{0}'.format(locale))
+            flag = QtGui.QIcon(':/flags/{0}'.format(locale))
             name = self._languages[locale]
             item = QtGui.QListWidgetItem(flag, name,
                                          self._available_languages_view)
@@ -151,15 +156,62 @@ class LanguagePage(QtGui.QWizardPage):
                 and self._chosen_languages.count() == 2)
 
     def get_selected_locales(self):
-        names = [self._chosen_languages.item(index).data(0)
+        names = [self._chosen_languages.item(index).data(QtCore.Qt.DisplayRole)
                  for index in range(self._chosen_languages.count())]
         inverted_languages = {value: key for key, value in self._languages}
         return [inverted_languages[name] for name in names]
 
+    def nextId(self):
+        if self.field('csv_type') or self.field('tsv_type'):
+            return ExportWizard.THIRD_FIELD_PAGE
 
-class FieldSelectionPage(QtGui.QWizardPage):
+
+class ThirdFieldSelectionPage(QtGui.QWizardPage):
     def __init__(self, parent):
-        super(FieldSelectionPage, self).__init__(parent)
+        super(ThirdFieldSelectionPage, self).__init__(parent)
+        self.property_name = None
+        self.setTitle(self.tr('Select which fields to export'))
+        schema = mdl.get_main_model().open_termbase.schema
+        # field selection widget
+        self._fields = QtGui.QListWidget(self)
+        for prop in schema.get_properties('T'):
+            item = QtGui.QListWidgetItem(prop.name, self._fields)
+            item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
+            item.setCheckState(QtCore.Qt.Unchecked)
+        self._fields.itemClicked.connect(self._handle_item_clicked)
+        # puts it all together
+        self.setLayout(QtGui.QVBoxLayout(self))
+        self.layout().addWidget(self._fields)
+
+    def initializePage(self):
+        # shows the correct subtitle
+        subtitle = self.tr('Please select exactly one field '
+                           'from the list below.')
+        if self.field('csv_type'):
+            subtitle += (self.tr('This will be exported as the third column '
+                                 ' in the resulting CSV file.'))
+        else:
+            subtitle += (self.tr('This will be exported as the third column '
+                                 'in the resulting TSV file.'))
+        self.setSubTitle(subtitle)
+
+    @QtCore.pyqtSlot(QtGui.QListWidgetItem)
+    def _handle_item_clicked(self, item):
+        for index in range(self._fields.count()):
+            current = self._fields.item(index)
+            if current is not item:
+                current.setCheckState(QtCore.Qt.Unchecked)
+        if item.checkState() == QtCore.Qt.Checked:
+            self.property_name = item.data(QtCore.Qt.DisplayRole)
+        else:
+            self.property_name = None
+        self.completeChanged.emit()
+
+    def isComplete(self):
+        return self.property_name is not None
+
+    def nextId(self):
+        return ExportWizard.FINAL_PAGE
 
 
 class FinalPage(QtGui.QWizardPage):
